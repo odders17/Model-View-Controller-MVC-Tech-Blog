aa4ca2ece38cfe5b9ca544b37d0e319d77ea0f8b
Paul Aston <odders17@gmail.com>
2021-09-07 16:42:15 +0100

    added js files

---
+++ models/comment.js
@@ -0,0 +1,54 @@
+const { Model, DataTypes } = require('sequelize');
+
+// Set up a config file
+const sequelize = require('../config/connection.js');
+
+class Comment extends Model { }
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    comment_text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id'
+      }
+    },
+    upvote: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    downvote: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+  }
+);
+
+module.exports = Comment;
---
+++ models/index.js
@@ -0,0 +1,33 @@
+const Comment = require("./comment");
+const Post = require("./post");
+const User = require("./user");
+
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+module.exports = {
+  Comment,
+  Post,
+  User,
+};
